97db4221874c333692762dc8da9cd720b460940c
DrFriday <wooshvoo@gmail.com>
2017-07-11 21:58:48 -0600

    adding stuff to index.js

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 
-
 var Alexa = require('alexa-sdk');
 
-console.log("I woked");
+
+var help_message = "";
+var help_reprompt = "";
+var stop_message = "";
+
+
+
+
+
+var handlers = {
+    'LaunchRequest': function () {
+        //When person launches this skill, it runs it
+        this.emit('CheckStreamersIntent');
+    },
+    'CheckSteamersIntent': function () {
+
+        //call twitch api and get streamer
+
+
+    },
+    //Each of the handlers with AMAZON are built in things that will automatically 
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = help_message;
+        var reprompt = help_reprompt
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', stop_message)
+    }
+};
